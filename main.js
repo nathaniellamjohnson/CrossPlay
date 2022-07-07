56732196e76aa4c68c49a8f5e2290e0e17b82425
Nathaniel L Johnson <nathanieljohnson009@gmail.com>
2022-07-06 20:18:57 -0700

    Working prototype of Spotify to intermediate file

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function requestAuthorization()
     url += "?client_id="+clientID;
     url += "&response_type=token";
     url += "&redirect_uri=" + encodeURI(redirect_uri)
-    url += "&scope=user-read-private playlist-read-private playlist-read-collaborative"
+    url += "&scope=user-read-private user-read-email playlist-read-private playlist-read-collaborative"
     url += "&show_dialog=true"
     url +=
         window.location.href = url; // Go to Spotify Authorization Screen with added
@@ -47,6 +47,7 @@ function checkHash()
     var url = new URL(urlString);
     return url.hash.includes("access_token=")
 }
+
 function handleAccessToken()
 {
     // gets string
@@ -69,3 +70,91 @@ function handleAccessToken()
 }
 
 
+function getUserInfo()
+{
+    var c = spotifyApi.getMe();
+    c.then(gotMe, gotMeError);
+}
+function gotMe(value)
+{
+
+    console.log(value);
+    document.getElementById("username").innerHTML = value.display_name;
+    document.getElementById("username").style.display = 'block';
+    document.getElementById("profileImage").src = value.images[0].url;
+    document.getElementById("profileImage").style.display = 'block';
+}
+function gotMeError(value)
+{
+    console.log(value);
+    alert(value);
+}
+
+function getUserPlaylists()
+{
+    var d = spotifyApi.getUserPlaylists();
+    d.then(gotPlaylist, gotPlaylistError)
+}
+function gotPlaylist(value)
+{
+    var a = "";
+    let tab = document.getElementById('playlistTable');
+    console.log(value);
+    for (var i = 0; i<value.items.length; i++)
+    {
+        var newRow = tab.insertRow(-1)
+        var firstCell = newRow.insertCell(0)
+        var newText = document.createTextNode(i+1);
+        firstCell.appendChild(newText);
+        var secondCell = newRow.insertCell(1);
+        newText = document.createTextNode(value.items[i].name);
+        secondCell.appendChild(newText);
+        var thirdCell = newRow.insertCell(2);
+        var btn = document.createElement('input');
+        btn.type = 'button';
+        btn.value = "Click Me!";
+        btn.id = value.items[i].id;
+        btn.onclick = function () {getPlaylistItems(this.id);};
+        thirdCell.appendChild(btn);
+    }
+    document.getElementById("playlistTable").style.display = 'block';
+}
+function gotPlaylistError (value)
+{
+    console.log(value);
+    alert(value);
+}
+
+
+function getPlaylistItems(id)
+{
+    console.log(id);
+    var e = spotifyApi.getPlaylistTracks(id);
+    e.then(gotPlaylistItems, gotPlaylistItemsError);
+}
+function gotPlaylistItems(value)
+{
+    console.log(value);
+    var arr = [];
+    for (var i = 0; i < value.items.length; i++)
+    {
+        if(value.items[i].track.type === "track")
+        {
+            console.log(value.items[i].track.type);
+            var f = "";
+            for (var j = 0; j < value.items[i].track.artists.length; j++)
+            {
+                f += value.items[i].track.artists[j].name;
+                f += " ";
+            }
+            var g = {songName:value.items[i].track.name, artistsName:f, externalID:value.items[i].track.external_ids.isrc };
+            arr.push(g);
+        }
+    }
+    console.log(arr);
+    localStorage.songs = JSON.stringify(arr);
+}
+function gotPlaylistItemsError(value)
+{
+    console.log(value);
+}
